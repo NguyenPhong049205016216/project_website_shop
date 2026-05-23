cc94ef80565e209844008a1c49b6b37725f9cfd8
nguyen phong <Phongn6216@ut.edu.vn>
2026-05-23 23:13:28 +0700

    sử dụng php trực tiếp nha không dùng html và css thuần nữa

---
+++ car-shop/admin/index.php
@@ -0,0 +1,18 @@
+<?php
+$title = "Car Shop";
+?>
+
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title><?php echo $title; ?></title>
+
+</head>
+
+<body>
+    <h1>Website bán xe</h1><br>
+    <p>Chào mừng bạn đến với trang quản trị của cửa hàng xe hơi!</p>
+
+</body>
+</html>
---
+++ car-shop/config/database.php
@@ -0,0 +1,14 @@
+<?php
+
+$host = "localhost";
+$user = "root";
+$password = "";
+$database = "car_shop";
+
+$conn = new mysqli($host, $user, $password, $database);
+
+if ($conn->connect_error) {
+    die("Kết nối thất bại: " . $conn->connect_error);
+}
+
+?>
