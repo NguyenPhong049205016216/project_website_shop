b7f841b7e9890d4bf7036bc1519bcb7367a580ba
nguyen phong <Phongn6216@ut.edu.vn>
2026-05-24 15:22:17 +0700

    làm trang chủ (index.html , và home.css) mô tả cách viết, run code bằng xampp cho nhóm.(để các bạn xem và làm theo nha)

--- car-shop/admin/index.php
+++ car-shop/admin/index.php
@@ -7,12 +7,39 @@ $title = "Car Shop";
 
 <head>
     <title><?php echo $title; ?></title>
-
+    <link rel="stylesheet" href="../assets/css/home.css">
 </head>
 
 <body>
-    <h1>Website bán xe</h1><br>
-    <p>Chào mừng bạn đến với trang quản trị của cửa hàng xe hơi!</p>
+    <div class="main">
+        <header>
+            <div class="logo">
+                <img src="../assets/images/cars/logo_cars_shop.png" alt="Logo">
+            </div>
+            <div class="search">
+                <form>
+                    <input type="text" id="search" placeholder="Tìm kiếm thông tin">
+                </form>
+            </div>
+            <div class="User">
+                <button>Đăng Ký</button>
+                <button>Đăng Nhập</button>
+            </div>
+        </header>
 
+        <nav>
+            <ul>
+                <li><a href="../index.php">Trang Chủ</a></li>
+                <li><a href="../cars.php">Xe Hơi</a></li>
+                <li><a href="../about.php">Giới Thiệu</a></li>
+                <li><a href="../services.php">Dịch Vụ</a></li>
+                <li><a href="../contact.php">Liên Hệ</a></li>
+            </ul>
+
+        </nav>>
+
+        </nav>
+    </div>
 </body>
+
 </html>
