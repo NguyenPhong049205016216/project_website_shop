0d98f3b5f436911ac0bb22a10618b17f2097785a
nguyen phong <Phongn6216@ut.edu.vn>
2026-05-26 00:38:09 +0700

    thêm cá thẻ class chứa thẻ link cho dao diệm tramg chủ chỉnh sữa css cho trang chủ

--- car-shop/index.php
+++ car-shop/index.php
@@ -30,20 +30,27 @@ $title = "Car Shop";
             <ul class="menu">
                 <li><a href="index.php">Trang Chủ</a>
                     <ul class="top_menu">
-                        <li><a href="#">Thông tin </a></li>
-                        <li><a href="#">Thông tin </a></li>
-                        <li><a href="#">Thông tin </a></li>
-                        <li><a href="#">Thông tin</a></li>
+                        <li><a href="#">introduce</a></li>
+                        <li><a href="#">cars new</a></li>
+                        <li><a href="#">best sell</a></li>
+                        <li><a href="#">promotion</a></li>
+                        <li><a href="#">contact</a></li>
                     </ul>
                 </li>
                 <li><a href="cars.php">Xe Hơi</a>
                     <ul class="sub_menu">
-
+                        <li><a href="#">Hảng xe</a></li>
+                        <li><a href="#">Loại xe</a></li>
+                        <li><a href="#">Giá xe</a></li>
+                        <li><a href="#">Tình trạng</a></li>
                     </ul>
                 </li>
                 <li><a href="wishlist.php">Wishlist</a>
-                    <ul class="">
-
+                    <ul class="bex_menu">
+                        <li><a href="#">save cars</a></li>
+                        <li><a href="#">liked recently</a></li>
+                        <li><a href="#">See list later</a></li>
+                        <li><a href="#">undersell</a></li>
                     </ul>
                 </li>
                 <li><a href="cart.php">Giỏ Hàng</a>
@@ -56,7 +63,7 @@ $title = "Car Shop";
 
         <section>
             <div class="logo_banner">
-                <img class="banner" src="assets/images/cars/banner_senter.png" alt="logo_banner">
+                <img class="banner" src="assets/images/cars/banner_senter_1.png" alt="logo_banner">
             </div>
 
         </section>
