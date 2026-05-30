e088060af9bfb7fbde6fd6af3a11ca7b6dd34b6a
nguyen phong <Phongn6216@ut.edu.vn>
2026-05-31 00:55:50 +0700

    Công việt, Fontend làm giao diện layout Index (chỉnh lại card Brands).

--- car-shop/index.php
+++ car-shop/index.php
@@ -55,8 +55,11 @@ $title = "Car Shop";
                     </ul>
                 </li>
                 <li><a href="cart.php">Giỏ Hàng</a>
-                    <ul>
-
+                    <ul class="footer_menu">
+                        <li><a href="#cart_items">Cart items</a></li>
+                        <li><a href="#checkout">Checkout</a></li>
+                        <li><a href="#payment">Payment</a></li>
+                        <li><a href="#order_history">Order history</a></li>
                     </ul>
                 </li>
             </ul>
@@ -71,14 +74,15 @@ $title = "Car Shop";
                 <img class="logo_xe" src="assets/images/cars/logo_banner.png"></img>
             </div>
         </section>
+
         <!-- Cars New -->
         <h2 class="chapter" id="cars_new">Cars New</h2>
         <main class="cars_introduce">
             <div class="item">
                 <img class="item_img" src="assets/images/cars/toyota_1.png" alt="Car 1">
                 <div class="item_info">
-                    <h3>Toyota GR Supra</h3>
-                    <p>Giá: $40,000</p>
+                    <h3>Toyota Vios</h3>
+                    <p>Giá: 545.000.000 VNĐ</p>
                     <a href="car_details.php?id=1" class="btn">Xem chi tiết</a>
                 </div>
             </div>
@@ -86,8 +90,8 @@ $title = "Car Shop";
             <div class="item">
                 <img class="item_img" src="assets/images/cars/toyota_2.png" alt="Car 1">
                 <div class="item_info">
-                    <h3>Mercedes-Benz S-Class</h3>
-                    <p>Giá: $110,000</p>
+                    <h3>Toyota Vios G  </h3>
+                    <p>Giá: 650.000.000 VNĐ</p>
                     <a href="car_details.php?id=1" class="btn">Xem chi tiết</a>
                 </div>
             </div>
@@ -95,52 +99,55 @@ $title = "Car Shop";
             <div class="item">
                 <img class="item_img" src="assets/images/cars/toyota_3.png" alt="Car 1">
                 <div class="item_info">
-                    <h3>Mercedes-Benz S-Class</h3>
-                    <p>Giá: $110,000</p>
+                    <h3>Vios 1.5G - CVT</h3>
+                    <p>Giá: 545.000.000 VNĐ</p>
                     <a href="car_details.php?id=1" class="btn">Xem chi tiết</a>
                 </div>
             </div>
 
             <div class="item">
-                <img class="item_img" src="assets/images/cars/toyota_1.png" alt="Car 1">
+                <img class="item_img" src="assets/images/cars/toyota_3(vang).png" alt="Car 1">
                 <div class="item_info">
-                    <h3>Mercedes-Benz S-Class</h3>
-                    <p>Giá: $110,000</p>
+                    <h3>Vios 1.5G - CVT</h3>
+                    <p>Giá: 550.000.000 VNĐ</p>
                     <a href="car_details.php?id=1" class="btn">Xem chi tiết</a>
                 </div>
             </div>
         </main>
         <!-- thương hiệu -->
-        <h1 class="chapter" id="Brands">Brands</h1>
+        <h1 class="chapter" id="Brands">Card Brands</h1>
         <main class="cars_brands">
             <div class="item">
-                <img class="item_img" src="assets/images/cars/icon_trademark_toyota.png" alt="trademark 1">
-                <div class="item_info">
-                    <h3>Thương hiệu family</h3>
-                </div>
+                <img class="img_brands" src="assets/images/cars/icon_trademark_toyota.png" alt="trademark 1">
             </div>
             <div class="item">
-                <img class="item_img" src="assets/images/cars/trademark_2.png" alt="trademark 1">
-                <div class="item_info">
-                    <h3>Thương hiệu luxury</h3>
-                </div>
+                <img class="img_brands" src="assets/images/cars/icon_Audi.png" alt="trademark 1">
             </div>
             <div class="item">
-                <img class="item_img" src="assets/images/cars/trademark_3.png" alt="trademark 1">
-                <div class="item_info">
-                    <h3>Thương hiệu sport</h3>
-                </div>
+                <img class="img_brands" src="assets/images/cars/icon_vinfast.png" alt="trademark 1">
             </div>
         </main>
 
         <footer>
-            <h2 class="chapter" id="footer">Contact</h2>
-            <cars_me class="contact">
-                <h2>Contact Us</h2>
+            <h1 class="chapter" id="footer">Contact</h1>
+            <div class="left_footer">
+                <h2>Cart me</h2>
                 <p>chuyên cung cấp các loại xe hơi chất lượng cao, từ hạng sang đến thể thao, với dịch vụ khách hàng tận tâm và giá cả cạnh tranh.</p>
+            </div>
+            <div class="center_footer">
+                <h2>Connect faster</h2>
+                <a href="index.php">Trang chủ</a>
+                <a href="car-details.php">Chi tiết xe</a>
+                <a href="wishlist.php">Wishlist</a>
+                <a href="cart.php">Giỏ hàng</a>
+                <a href="cars.php">Xe hơi</a>
+            </div>
+            <div class="right_footer">
+                <h2>Contact Us</h2>
+                <p>https://www.cars.me.com</p>
                 <p>Email: phong@example.com</p>
                 <p>Phone: 123-456-7890</p>
-            </cars_me>
+            </div>
         </footer>
     </div>
 </body>
