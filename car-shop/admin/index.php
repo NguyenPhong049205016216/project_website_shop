a48cf8bb5a43072cfad307f082a343d2ca1a1f31
nguyen phong <Phongn6216@ut.edu.vn>
2026-07-06 06:59:54 +0700

    cập nhập lần cuối

--- car-shop/admin/index.php
+++ car-shop/admin/index.php
@@ -21,12 +21,31 @@
     </header>
     <div class="container">
         <left class="sidebar">
-            <a href="/car-shop/admin/dashboard.php">Dashboard</a>
-            <a href="/car-shop/admin/cars.php">Cars</a>
-            <a href="/car-shop/admin/users.php">Users</a>
-            <a href="/car-shop/admin/brands.php">brands</a>
-            <a href="/car-shop/admin/orders.php">Orders</a>
-            <button class="btn" href="/car-shop/index.php">Logout</button> 
+            <a href="/car-shop/admin/dashboard.php">
+                <img src="/car-shop/assets/images/icon/icon-doanhthutong.png" class="sidebar-icon">
+                <span>Dashboard</span>
+            </a>
+
+            <a href="/car-shop/admin/cars.php">
+                <img src="/car-shop/assets/images/icon/ô-tô-3d.png" class="sidebar-icon">
+                <span>Cars</span>
+            </a>
+
+            <a href="/car-shop/admin/users.php">
+                <img src="/car-shop/assets/images/icon/nguoi-dung.png" class="sidebar-icon">
+                <span>Users</span>
+            </a>
+
+            <a href="/car-shop/admin/brands.php">
+                <img src="/car-shop/assets/images/icon/icon-danhmuc.png" class="sidebar-icon">
+                <span>brands</span>
+            </a>
+
+            <a href="/car-shop/admin/orders.php">
+                <img src="/car-shop/assets/images/icon/icon-order.png" class="sidebar-icon">
+                <span>Orders</span>
+            </a>
+            <button class="btn" href="/car-shop/index.php">Logout</button>
         </left>
     </div>
 </body>
